cded7597548d45de008e887162ed1123c5d79bba
Jiayi Qu <jyqqu@ucdavis.edu>
2022-03-22 11:42:10 -0700

    halfway modifing GBLUP with APY

--- test/test_APY.jl
+++ test/test_APY.jl
@@ -8,17 +8,23 @@ first(phenotypes,5)
 IDs = phenotypes[!,:ID][1:50]
 
 genotypes  = get_genotypes(genofile,separator=',',method="GBLUP", name4core = IDs);
-genotypes.genotypes
-genotypes.APYinfo
+#genotypes  = get_genotypes(genofile,separator=',',method="GBLUP");
 
 # Step 3: Build Model Equations
 model_equation  ="y1 = intercept + x1 + x2 + genotypes"
 model = build_model(model_equation);
+model.M[1].genetic_variance
+model.M[1].G
+model.M[1].π
+model.M[1].obsID
+model.obsID
+model.M[1].APYinfo
+model.M[1].genotypes
+
 # Step 4: Set Factors or Covariates
 set_covariate(model,"x1");
 # Step 5: Set Random or Fixed Effects
 set_random(model,"x2");
-
 
 # Step 6: Run Analysis
 out=runMCMC(model,phenotypes);
